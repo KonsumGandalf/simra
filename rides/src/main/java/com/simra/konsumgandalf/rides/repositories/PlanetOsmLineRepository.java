d82bcf872694cb9a126449383f7fa0681ee9b5ad
KonumGandalf <david_schmidt_privat@outlook.com>
2024-12-17 16:28:45 +0100

    feat(rides): PlanetOsmLineRepository
    Add query method for getting of osm_id

--- rides/src/main/java/com/simra/konsumgandalf/rides/repositories/PlanetOsmLineRepository.java
+++ rides/src/main/java/com/simra/konsumgandalf/rides/repositories/PlanetOsmLineRepository.java
@@ -3,6 +3,7 @@ package com.simra.konsumgandalf.rides.repositories;
 import com.simra.konsumgandalf.common.models.entities.PlanetOsmLine;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,6 +11,6 @@ import java.util.List;
 @Repository
 public interface PlanetOsmLineRepository extends JpaRepository<PlanetOsmLine, Long> {
     @Query("SELECT p FROM PlanetOsmLine p WHERE p.osm_id IN :osmIds")
-    public List<PlanetOsmLine> findAllByOsmId(List<Long> osmIds);
+    public List<PlanetOsmLine> findAllByOsmId(@Param("osmIds") List<Long> osmIds);
 
 }
