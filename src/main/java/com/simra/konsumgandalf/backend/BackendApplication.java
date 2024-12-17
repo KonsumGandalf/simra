c05a8584875febe33650f1253d73cfb24f7504a0
KonumGandalf <david_schmidt_privat@outlook.com>
2024-12-17 16:29:20 +0100

    feat(backend): CorsConfig
    Add cors config to query from chrome.

--- src/main/java/com/simra/konsumgandalf/backend/BackendApplication.java
+++ src/main/java/com/simra/konsumgandalf/backend/BackendApplication.java
@@ -5,17 +5,22 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.stereotype.Controller;
 
 @SpringBootApplication
 @EnableJpaRepositories(basePackages = {
-		"com.simra.konsumgandalf.rides.repositories"
+		"com.simra.konsumgandalf.rides.repositories",
+		"com.simra.konsumgandalf.osmPlanet.repositories"
 })
 @ComponentScan(basePackages = {
 		"com.simra.konsumgandalf.osmPlanet.initializers",
 		"com.simra.konsumgandalf.common.utils.services",
 		"com.simra.konsumgandalf.rides.controllers",
 		"com.simra.konsumgandalf.rides.services",
-		"com.simra.konsumgandalf.osmrBackend.services"
+		"com.simra.konsumgandalf.osmrBackend.services",
+		"com.simra.konsumgandalf.osmPlanet.services",
+		"com.simra.konsumgandalf.osmPlanet.controller",
+		"com.simra.konsumgandalf.backend.config"
 })
 @EntityScan(basePackages = {
 		"com.simra.konsumgandalf.rides.models.entities",
