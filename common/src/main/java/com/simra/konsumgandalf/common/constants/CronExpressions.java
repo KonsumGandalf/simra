54cf71ce344b8b7fb4c4628d15a238028b664e7f
David Schmidt <66205241+KonsumGandalf@users.noreply.github.com>
2024-12-28 00:04:47 +0100

    feat(common): CronExpressions
    Add daily schedule expression

---
+++ common/src/main/java/com/simra/konsumgandalf/common/constants/CronExpressions.java
@@ -0,0 +1,10 @@
+package com.simra.konsumgandalf.common.constants;
+
+/**
+ * Defines reusable cron expressions.
+ */
+public class CronExpressions {
+
+	public static final String EVERY_DAY = "0 0 0 * * ?";
+
+}
