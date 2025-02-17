6de75d42e87eca4d5bc2f594f1604eae86aa995c
David Schmidt <66205241+KonsumGandalf@users.noreply.github.com>
2025-02-17 16:43:35 +0100

    feat(osmPlanet): services, controller
    Integrate endpoint to query for safety metrics of streets

--- osmPlanet/src/main/java/com/simra/konsumgandalf/osmPlanet/controller/StreetsController.java
+++ osmPlanet/src/main/java/com/simra/konsumgandalf/osmPlanet/controller/StreetsController.java
@@ -4,30 +4,28 @@ import com.simra.konsumgandalf.common.models.enums.TrafficTimes;
 import com.simra.konsumgandalf.common.models.enums.WeekDays;
 import com.simra.konsumgandalf.osmPlanet.services.OsmHighwayService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("streets")
+@Validated
 public class StreetsController {
 
 	@Autowired
 	private OsmHighwayService osmHighwayService;
 
-	@GetMapping("")
+	@GetMapping("/grid")
 	public List<Map<String, Object>> getHighwayInformation(@RequestParam("lat") double lat,
 															   @RequestParam("lng") double lng, @RequestParam("zoom") int zoom,
 															   @RequestParam(defaultValue = "ALL_DAY") TrafficTimes trafficTime,
 															   @RequestParam(defaultValue = "ALL_WEEK") WeekDays weekDay) {
 		return osmHighwayService.getHighwayInformation(lat, lng, zoom, trafficTime, weekDay);
 	}
-
 }
