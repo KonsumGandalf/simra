b307b046ef53fdc3b4d77e9755c5069729df5de8
David Schmidt <66205241+KonsumGandalf@users.noreply.github.com>
2025-03-02 16:01:55 +0100

    feat(osmPlanet): services, controller
    Add 2 endpoints to query rides of street within time range and details of street with tags

---
+++ osmPlanet/src/main/java/com/simra/konsumgandalf/osmPlanet/classes/dtos/RideEntityDTO.java
@@ -0,0 +1,8 @@
+package com.simra.konsumgandalf.osmPlanet.classes.dtos;
+
+import java.util.Date;
+
+public interface RideEntityDTO {
+	public Date getRideStart();
+	public Date getRideEnd();
+}
--- osmPlanet/src/main/java/com/simra/konsumgandalf/osmPlanet/services/OsmHighwayService.java
+++ osmPlanet/src/main/java/com/simra/konsumgandalf/osmPlanet/services/OsmHighwayService.java
@@ -1,16 +1,24 @@
 package com.simra.konsumgandalf.osmPlanet.services;
 
+import com.simra.konsumgandalf.common.models.entities.PlanetOsmLine;
 import com.simra.konsumgandalf.common.models.enums.TrafficTimes;
 import com.simra.konsumgandalf.common.models.enums.WeekDays;
+import com.simra.konsumgandalf.osmPlanet.classes.dtos.RideEntityDTO;
 import com.simra.konsumgandalf.osmPlanet.classes.enums.RoadTypes;
 import com.simra.konsumgandalf.osmPlanet.classes.mapper.ZoomDistanceMapper;
 import com.simra.konsumgandalf.osmPlanet.classes.mapper.ZoomRoadTypeMapper;
 import com.simra.konsumgandalf.osmPlanet.repositories.OsmHighwayRepository;
+import org.modelmapper.internal.bytebuddy.asm.Advice;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.xml.crypto.Data;
+import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 @Service
 public class OsmHighwayService {
@@ -29,6 +37,14 @@ public class OsmHighwayService {
 
 		return osmHighwayRepository.findHighways(lng, lat, distanceFilter, roadTypes, 0.0001, trafficTime.name(),
 				weekDay.name());
+	}
+
+	public Optional<PlanetOsmLine> getHighwayById(long id) {
+		return osmHighwayRepository.findById(id);
+	}
+
+	public List<RideEntityDTO> getRideEntitiesTimeById(long id, LocalDateTime startTime, LocalDateTime endTime) {
+		return osmHighwayRepository.findRideEntitiesTimeById(id, startTime, endTime);
 	}
 
 }
