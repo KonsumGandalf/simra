2151e4eedb5d3fb4596be2c8d2005ac64f81e767
David Schmidt <66205241+KonsumGandalf@users.noreply.github.com>
2025-03-27 10:30:41 +0100

    refactor(osmPlanet): classes
    Fix naming of simra regions and ensure in list that a street is within a boundary.

--- osmPlanet/src/main/java/com/simra/konsumgandalf/osmPlanet/classes/specifications/SafetyMetricsGenericSpecification.java
+++ osmPlanet/src/main/java/com/simra/konsumgandalf/osmPlanet/classes/specifications/SafetyMetricsGenericSpecification.java
@@ -1,13 +1,14 @@
 package com.simra.konsumgandalf.osmPlanet.classes.specifications;
 
-import com.simra.konsumgandalf.common.models.entities.SafetyMetricsRegion;
 import com.simra.konsumgandalf.common.models.enums.TrafficTimes;
 import com.simra.konsumgandalf.common.models.enums.WeekDays;
 import jakarta.persistence.criteria.CriteriaBuilder;
 import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Expression;
 import jakarta.persistence.criteria.Join;
 import jakarta.persistence.criteria.Predicate;
 import jakarta.persistence.criteria.Root;
+import org.locationtech.jts.geom.Geometry;
 import org.springframework.data.jpa.domain.Specification;
 
 import java.util.ArrayList;
@@ -33,12 +34,11 @@ public class SafetyMetricsGenericSpecification {
 	 */
 	public static <T> Specification<T> filterBy(String joinField, Long id, String name, List<String> highwayType,
 			Float minDangerousScore, Float maxDangerousScore, Integer minNumberOfRides, Integer minNumberOfIncidents,
-			List<TrafficTimes> trafficTime, List<WeekDays> weekDay, List<Integer> year) {
+			List<TrafficTimes> trafficTime, List<WeekDays> weekDay, List<Integer> year, Geometry regionWay) {
 
 		return (Root<T> root, CriteriaQuery<?> query, CriteriaBuilder cb) -> {
 			List<Predicate> predicates = new ArrayList<>();
 
-			// Perform a join on the provided join field
 			Join<T, ?> join = root.join(joinField);
 
 			if (id != null) {
@@ -74,6 +74,10 @@ public class SafetyMetricsGenericSpecification {
 			if (year != null) {
 				predicates.add(root.get("year").in(year));
 			}
+			if (regionWay != null) {
+				Expression<Object> regionBoundary = cb.literal(regionWay);
+				predicates.add(cb.isTrue(cb.function("ST_INTERSECTS", Boolean.class, join.get("way"), regionBoundary)));
+			}
 
 			return cb.and(predicates.toArray(new Predicate[0]));
 		};
@@ -84,7 +88,7 @@ public class SafetyMetricsGenericSpecification {
 			List<WeekDays> weekDay, List<Integer> year) {
 
 		return SafetyMetricsGenericSpecification.filterBy(joinField, null, name, null, minDangerousScore, null,
-				minNumberOfRides, minNumberOfIncidents, trafficTime, weekDay, year);
+				minNumberOfRides, minNumberOfIncidents, trafficTime, weekDay, year, null);
 	}
 
 }
